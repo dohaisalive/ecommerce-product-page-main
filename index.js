8e69153de065151d45b0e5fcf2585d933982d31e
dohaisalive <dohaisalive@gmail.com>
2022-04-16 14:42:24 +0300

    modified html and css an js

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 let counter = 0;
 let cart = [];
-let item = "";
+let first = true;
+
+class Item {
+  constructor(
+    companyName,
+    productName,
+    description,
+    price,
+    sale,
+    ogPrice,
+    mainPic
+  ) {
+    this.companyName = companyName;
+    this.productName = productName;
+    this.description = description;
+    this.price = price;
+    this.sale = sale;
+    this.ogPrice = ogPrice;
+    this.mainPic = mainPic;
+  }
+}
+
+let sneakers = new Item(
+  "Sneaker Company",
+  "Fall Limited Edition Sneakers",
+  "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+  125.0,
+  50,
+  250.0,
+  "./images/image-product-1-thumbnail.jpg"
+);
+
 function showItems() {
   var x = document.getElementById("cartContainer");
+
   if (x.style.display === "none") {
     x.style.display = "block";
+    showCart = true;
   } else {
     x.style.display = "none";
+    showCart = true;
   }
 }
 
@@ -22,5 +56,22 @@ function plus() {
 }
 
 function addToCart() {
-  cart.add(item);
+  if (counter !== 0) {
+    if (first === true) {
+      first = false;
+      var img = document.createElement("img");
+      img.src = sneakers.mainPic;
+      var src = document.getElementById("productPic");
+      src.appendChild(img);
+    }
+
+    document.getElementById("productName").innerHTML = sneakers.productName;
+    document.getElementById("price").innerHTML = `$${sneakers.price}`;
+    document.getElementById("itemQuantity").innerHTML = `x ${counter}`;
+    document.getElementById("overall").innerHTML = `$${
+      counter * sneakers.price
+    }`;
+  }
 }
+
+function renderCart(item) {}
